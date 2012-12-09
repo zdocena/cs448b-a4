cb485cc910379e054f068c01f860ae86190d9cae
Zahara Docena <zahara.docena@gmail.com>
2012-12-08 20:11:57 -0800

    translates xml to json.

---
+++ graphmlParser.py
@@ -0,0 +1,31 @@
+# import xml parser
+from xml.dom.minidom import parseString
+from xml.etree import ElementTree as ET
+
+f = open('input_files/greek2.graphml', 'r')
+data = f.read()
+f.close()
+dom = parseString(data)
+jsonArray = []
+
+for node in dom.getElementsByTagName('node'):
+  json = {"x": "",
+          "y": "",
+          "title": "",
+          "date": ""}
+          
+  # iterate over node tags
+  for data_elem in node.getElementsByTagName('data'):
+    attr = data_elem.attributes["key"].value
+    
+    if attr == "label":
+      attr = "title"
+    
+    # add data value to json object
+    if attr in json:
+      json[attr] = data_elem.firstChild.nodeValue
+
+  jsonArray.append(json)
+
+for x in jsonArray:
+  print x
---
+++ js/graphmlParser.js
@@ -0,0 +1,5 @@
+var inputFile = "";
+
+var parseGraphmlFile = function() {
+  
+}
