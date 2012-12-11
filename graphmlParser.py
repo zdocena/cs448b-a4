03de5f3c48b143e6886a1d64a4935504c0ca3c51
Zahara Docena <zahara.docena@gmail.com>
2012-12-10 20:43:10 -0800

    saving minor changes

--- graphmlParser.py
+++ graphmlParser.py
@@ -8,6 +8,7 @@ f.close()
 dom = parseString(data)
 jsonArray = []
 
+# get article nodes
 for node in dom.getElementsByTagName('node'):
   json = {"x": "",
           "y": "",
@@ -17,15 +18,14 @@ for node in dom.getElementsByTagName('node'):
   # iterate over node tags
   for data_elem in node.getElementsByTagName('data'):
     attr = data_elem.attributes["key"].value
-    
     if attr == "label":
       attr = "title"
     
     # add data value to json object
     if attr in json:
       json[attr] = data_elem.firstChild.nodeValue
-
+      
   jsonArray.append(json)
-
+  
 for x in jsonArray:
   print x
